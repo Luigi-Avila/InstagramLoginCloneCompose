461ee3a32c944509290e446942197f9cd301ee9c
Luigi-Avila <l.avila.uvg@gmail.com>
2023-01-26 16:40:18 -0600

    feat: button's logic added

--- app/src/main/java/com/example/instagramloginclonecompose/LoginScreen.kt
+++ app/src/main/java/com/example/instagramloginclonecompose/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.instagramloginclonecompose
 
 import android.app.Activity
+import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -52,9 +53,15 @@ fun Body(modifier: Modifier) {
     Column(modifier = modifier) {
         ImageLogo(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
-        Email(email) { email = it }
+        Email(email) {
+            email = it
+            isLoginEnable = loginEnable(email, password)
+        }
         Spacer(modifier = Modifier.size(4.dp))
-        Password(password) { password = it }
+        Password(password) {
+            password = it
+            isLoginEnable = loginEnable(email, password)
+        }
         Spacer(modifier = Modifier.size(8.dp))
         ForgotPassword(Modifier.align(Alignment.End))
         Spacer(modifier = Modifier.size(16.dp))
@@ -245,3 +252,6 @@ fun Header(modifier: Modifier) {
             activity.finish()
         })
 }
+
+fun loginEnable(email: String, password: String) =
+    Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
